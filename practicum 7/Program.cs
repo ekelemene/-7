17519bdf9a83bc2271ecb8894530a915dd7f5638
ekelemene <ksuxaskrip2007@gmail.com>
2024-11-04 21:54:08 +0200

    Add files via upload

---
+++ practicum 7/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicum_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1. вывести в столбик 10 раз слово "Привет";
+            //int i = 0;
+            //while (i<10)
+            //{
+            //    Console.WriteLine("Привет");
+            //    i++;
+            //}
+
+            //int i = 0;
+            //do
+            //{
+            //    Console.WriteLine("Hi");
+            //    i++;
+            //}
+            //while (i < 10);
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Hi");
+            //}
+
+            //Задание 2. вывести в столбик первые 10 натуральных чисел;
+            //int i = 1;
+            //while (i <=10)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //int i = 1;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //while (i<11);
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Задание 3. вывести в столбик первые 5 натуральных чисел в обратном порядке;
+            //int i = 5;
+            //while (i >= 1)
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+
+            //int i = 5;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+            //while (i>=1);
+
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Задание 4. вывести на экран целые числа из интервала от 1 до n;
+            //Console.Write("Ввдите n = ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int i = 1;
+            //while (i<=n)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //Console.Write("Ввдите n = ");
+            //int n = Convert.ToInt32(Console.ReadLineI());
+            //int i = 1;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //while (i<=n);
+
+            //Console.Write("Ввдите n = ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Задание 5. запросить у пользователя 5 слов, после чего вывести вначале дважды каждое нечетное слово и затем
+            //трижды каждое четное слово, между выводимыми словами вставить разделитель в виде трех символьных пробелов (“   ”).
+            //string[] words = new string[5];
+            //Console.WriteLine("введите 5 слов =");
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    words[i] = Console.ReadLine();
+            //}
+
+            //for (int i = 5 - 1; i >= 0; i--)
+            //{
+            //    string result=(i % 2 == 0) ? $"{words[i]}   {words[i]}" : $"{words[i]}  {words[i]}  {words[i]}";
+            //    Console.WriteLine(result);
+            //}
+            
+
+
+                
+
+
+
+
+        }
+    }
+}
---
+++ practicum 7/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("practicum 7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("practicum 7")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("884ed3dd-60f5-4f7e-aff8-15903d4daf09")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
